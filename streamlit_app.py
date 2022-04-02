c761f8308049777afe7e09f83f18fec67dac92c7
to-schi <torschilling@gmail.com>
2022-04-02 11:33:24 +0200

    github link

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,7 +36,7 @@ st.markdown(
 This demo app is using a simplified end-to-end speech recognition engine based on DeepSpeech2.
 It was trained on the LibriSpeech dataset with 100 hours of English speech. The word-error-rate is currently at 19%.
 A good microphone is recommended.\n
-More information can be found [here](https://github.com/to-schi).
+More information can be found [here](https://github.com/to-schi/asr-deepspeech2-webdemo).
 """
 )
 # coloring "st.buttons" in main page light blue:
