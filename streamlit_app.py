f88f60c56f3cffdadd5ee90d2fb3533500beb8ca
to-schi <torschilling@gmail.com>
2022-04-02 16:52:12 +0200

    text changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,8 +34,8 @@ st.title('Speech Recognition Demo')
 st.markdown(
     """
 This demo app is using a simplified end-to-end speech recognition engine based on DeepSpeech2.
-It was trained on the LibriSpeech dataset with 100 hours of English speech. The word-error-rate is currently at 19%.
-A good microphone is recommended.\n
+It was trained on the LibriSpeech dataset with 100 hours of English speech and is work in progress. 
+The word-error-rate is currently at 19%. A good microphone is recommended.\n
 More information can be found [here](https://github.com/to-schi/asr-deepspeech2-webdemo).
 """
 )
